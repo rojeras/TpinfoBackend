fd8479758b6a70a9575200a2e563b5401d298e9e
Leo <lars.erik.rojeras@skoview.se>
2020-12-14 11:43:36 +0100

    Added label and stop command to run script

--- bin/docker-run-backend.sh
+++ bin/docker-run-backend.sh
@@ -2,12 +2,12 @@
 if [ "$#" -ne "2" ]; then
 	echo "Sepecify two parameters:"
 	echo
-	echo "       $0 tag port"
+	echo "       $0 image port"
 	echo
 	exit 1;
 fi
 
-tag="$1"
+image="$1"
 port="$2"
 
 #ENVIRFILE=src/backend-envir.lst
@@ -21,7 +21,7 @@ export DBSERVER=localhost
 #export DBSERVER=localhost 
 export DBSERVER=host.docker.internal 
 
-docker container stop $(docker container ls -q --filter name=rojeras/tpinfo-backend*)
-docker run --env-file=bin/backend-envir.lst -d -e DBSERVER -e DBUSER -e DBPWD -e DBNAME -p $port:80 $tag
+docker container stop $(docker container ls -q --filter "label=layer=backend")
+docker run --env-file=bin/backend-envir.lst -d -e DBSERVER -e DBUSER -e DBPWD -e DBNAME -p $port:80 $image
 
 
