eb76545add56180e1d5161724c61c14ebd3c153a
Leo <lars.erik.rojeras@skoview.se>
2021-03-09 08:54:57 +0100

    local-run-backend.sh works

--- bin/local-run-backend.sh
+++ bin/local-run-backend.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-TOPDIR=/home/leo/Documents/data/Eternal/development/tpinfo
-ENVIRFILE=$TOPDIR/backend/bin/backend-envir.lst
+CURRENTDIR=$(pwd)
 
-cd $TOPDIR
-mkdir public_html 2> /dev/null
-cd public_html
+TOPDIR=/home/leo/tmp/dev/tpinfo-backend
+ENVIRFILE=/home/leo/Documents/protected/security/backend-envir.lst
+RUNDIR=/home/leo/tmp/runbackend
+
+mkdir $RUNDIR 2> /dev/null
+cd $RUNDIR
 
 rm tpdb
-ln -s ../backend/src tpdb 2> /dev/null
+
+ln -s $TOPDIR/src tpdb 2> /dev/null
 ln -s /tmp/history history 2> /dev/null
-ln -s ../../statapicache/active statapifiles 2> /dev/null
+# ln -s ../../statapicache/active statapifiles 2> /dev/null
 
 set -o allexport
 source $ENVIRFILE
 set +o allexport
 # export DBSERVER=localhost
 
-echo "Use http://localhost:5555/hippo/hippo.html or http://localhost:5555/stat/statistics.html"
-echo
+# echo "Use http://localhost:5555/hippo/hippo.html or http://localhost:5555/stat/statistics.html"
+echo "Backend running and listening to port 5555"
 
 php -S localhost:5555 -t .
 
+cd $CURRENTDIR
 
